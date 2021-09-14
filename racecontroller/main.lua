8ef0e882d6c47225d35bbd8843b28770bc7f53cf
Matt <rcheeze1337@gmail.com>
2021-09-13 22:27:11 -0400

    Initial commit

---
+++ racecontroller/main.lua
@@ -0,0 +1,242 @@
+RaceController = RaceController or {}
+RaceController.racers = {}
+RaceController.running = false
+dofile("./addons/racecontroller/utils.lua")
+
+RaceController.readCfg()
+
+local f = io.open("./config.json")
+RaceController.currentMap = RaceController.splitString(decode_json(f:read("*all")).map, "/")[2]
+f:close()
+
+hooks.register("OnStdIn", "consoleControl", function(input)
+    local cmd = RaceController.splitString(input)
+
+    if cmd[1] == "/reloadcfg" then
+        RaceController.readCfg()
+    end
+
+    if cmd[1] == "/writecfg" then
+        RaceController.writeCfg()
+    end
+
+    if cmd[1] == "/promote" and cmd[2] then
+        local ply = RaceController.findPlayerByName(cmd[2])
+
+        if ply then
+            if RaceController.cfg.admins[ply:getSecret()] then
+                RaceController.print("Player " .. ply:getName() .. " is already admin")
+            else
+                RaceController.cfg.admins[ply:getSecret()] = true
+                RaceController.print("Promoted " .. ply:getName() .. " to admin")
+            end
+        else
+            RaceController.print("Player not found")
+        end
+    end
+
+    if cmd[1] == "/demote" and cmd[2] then
+        local ply = RaceController.findPlayerByName(cmd[2])
+
+        if ply then
+            if not RaceController.cfg.admins[ply:getSecret()] then
+                RaceController.print("Player " .. ply:getName() .. " is not admin")
+            else
+                RaceController.cfg.admins[ply:getSecret()] = nil
+                RaceController.print("Demoted " .. ply:getName() .. " from admin")
+            end
+        else
+            RaceController.print("Player not found")
+        end
+    end
+end)
+
+hooks.register("OnChat", "chatControl", function(clientID, message)
+    if string.sub(message, 1, 1) ~= "/" then return end
+    if not RaceController.isAdmin(connections[clientID]) then return end
+    local cmd = RaceController.splitString(message)
+    local caller = connections[clientID]
+    local callerPos
+
+    --Required due to bug
+    --https://github.com/TheHellBox/KISS-multiplayer/issues/104
+    if not pcall(function()
+        callerPos = vehicles[caller:getCurrentVehicle()]:getTransform():getPosition()
+    end) then
+        RaceController.msg(caller, "WARNING: Your vehicle object is nil! Spawn a new vehicle before running any commands!")
+
+        return ""
+    end
+
+    if cmd[1] == "/add" then
+        local ply = RaceController.findPlayerByName(cmd[2])
+
+        if not cmd[2] or not ply then
+            RaceController.msg(caller, "Player not found")
+        elseif RaceController.racers[ply:getID()] then
+            RaceController.msg(caller, "Player '" .. ply:getName() .. "'already added")
+        elseif not vehicles[ply:getCurrentVehicle()] then
+            RaceController.msg(caller, "WARNING: Player '" .. ply:getName() .. "' has nil vehicle!")
+        else
+            RaceController.addRacer(ply:getID())
+            RaceController.msg(caller, "Added player " .. ply:getName() .. " in vehicle " .. vehicles[ply:getCurrentVehicle()]:getData():getName())
+        end
+    end
+
+    if cmd[1] == "/remove" then
+        local ply = RaceController.findPlayerByName(cmd[2])
+
+        if not cmd[2] or not ply then
+            RaceController.msg(caller, "Player not found")
+        else
+            if RaceController.racers[ply:getID()] then
+                RaceController.removeRacer(ply:getID())
+                RaceController.msg(caller, "Removed player '" .. ply:getName() .. "'")
+            else
+                RaceController.msg(caller, "Player not added")
+            end
+        end
+    end
+
+    if cmd[1] == "/reloadcfg" then
+        RaceController.readCfg()
+        RaceController.msg(caller, "Read configuration file successfully")
+    end
+
+    if cmd[1] == "/writecfg" then
+        RaceController.writeCfg()
+        RaceController.msg(caller, "Wrote configuration file successfully")
+    end
+
+    if cmd[1] == "/resetcfg" then
+        RaceController.loadDefaultCfg()
+        RaceController.msg(caller, "Reset active configuration to default - /writecfg to save")
+    end
+
+    if cmd[1] == "/p1" then
+        if not RaceController.cfg.linePositions[RaceController.currentMap] then
+            RaceController.cfg.linePositions[RaceController.currentMap] = {}
+        end
+
+        RaceController.cfg.linePositions[RaceController.currentMap].x1 = math.floor(callerPos[1] * 10) / 10
+        RaceController.cfg.linePositions[RaceController.currentMap].y1 = math.floor(callerPos[2] * 10) / 10
+        RaceController.msg(caller, "Set position 1 to (" .. RaceController.cfg.linePositions[RaceController.currentMap].x1 .. ", " .. RaceController.cfg.linePositions[RaceController.currentMap].y1 .. ")")
+    end
+
+    if cmd[1] == "/p2" then
+        if not RaceController.cfg.linePositions[RaceController.currentMap] then
+            RaceController.cfg.linePositions[RaceController.currentMap] = {}
+        end
+
+        RaceController.cfg.linePositions[RaceController.currentMap].x2 = math.floor(callerPos[1] * 10) / 10
+        RaceController.cfg.linePositions[RaceController.currentMap].y2 = math.floor(callerPos[2] * 10) / 10
+        RaceController.msg(caller, "Set position 2 to (" .. RaceController.cfg.linePositions[RaceController.currentMap].x2 .. ", " .. RaceController.cfg.linePositions[RaceController.currentMap].y2 .. ")")
+    end
+
+    if cmd[1] == "/cfg" and cmd[2] then
+        if not RaceController.cfg[cmd[2]] then
+            RaceController.msg(caller, "Configuration option not found")
+        else
+            if not cmd[3] then
+                if type(RaceController.cfg[cmd[2]]) == "number" or type(RaceController.cfg[cmd[2]]) == "string" then
+                    RaceController.msg(caller, cmd[2] .. " == " .. RaceController.cfg[cmd[2]])
+                else
+                    RaceController.msg(caller, "Can only display string or number configuration settings")
+                end
+            else
+                local setting = tonumber(cmd[3])
+
+                if not setting or setting < 0 then
+                    RaceController.msg(caller, "Configuration option must be a valid positive number")
+                else
+                    RaceController.cfg[cmd[2]] = setting
+                    RaceController.msg(caller, "'" .. cmd[2] .. "' set to " .. cmd[3])
+                end
+            end
+        end
+    end
+
+    if cmd[1] == "/start" then
+        if RaceController.tableEmpty(RaceController.racers) then
+            RaceController.msg(caller, "No players added!")
+
+            return ""
+        end
+
+        if not RaceController.cfg.linePositions[RaceController.currentMap] or not RaceController.cfg.linePositions[RaceController.currentMap].x1 or not RaceController.cfg.linePositions[RaceController.currentMap].x2 then
+            RaceController.msg(caller, "No finish line defined for this map!")
+
+            return ""
+        end
+
+        local n = 5
+
+        RaceController.timer("countdown", 1, 6, function()
+            if n > 0 then
+                RaceController.broadcast("Race starting in " .. n)
+            else
+                RaceController.broadcast("GO!")
+                RaceController.running = true
+                RaceController.startTime = os.clock()
+
+                for _, racerData in pairs(RaceController.racers) do
+                    racerData.lastLapClock = os.clock()
+                end
+            end
+
+            n = n - 1
+        end)
+    end
+
+    if cmd[1] == "/reset" then
+        RaceController.running = false
+        RaceController.racers = {}
+        RaceController.timers = {}
+        RaceController.broadcast("Script reset")
+    end
+
+    return ""
+end)
+
+local lastNilVehicleWarn = os.clock()
+
+--RaceController.timer("interval", 0.1, 0, function()
+hooks.register("Tick", "interval", function()
+    if not RaceController.running then return end
+
+    for racerID, racerData in pairs(RaceController.racers) do
+        if vehicles[connections[racerID]:getCurrentVehicle()] then
+            local racer = connections[racerID]
+            local vehiclePos = vehicles[racer:getCurrentVehicle()]:getTransform():getPosition()
+            local linePos = RaceController.cfg.linePositions[RaceController.currentMap]
+            local distToFinish = RaceController.pointToLineDist(vehiclePos[1], vehiclePos[2], linePos.x1, linePos.y1, linePos.x2, linePos.y2)
+
+            if distToFinish < RaceController.cfg.finishLineWidth and os.clock() - racerData.lastLapClock > RaceController.cfg.minLapTime and racerData.lap <= RaceController.cfg.laps then
+                racerData.lap = racerData.lap + 1
+
+                if racerData.lap <= RaceController.cfg.laps then
+                    RaceController.broadcast(racer:getName() .. " is on lap " .. racerData.lap)
+                end
+
+                racerData.lastLapTime = os.clock() - racerData.lastLapClock
+
+                if racerData.lastLapTime < racerData.fastestLap then
+                    racerData.fastestLap = racerData.lastLapTime
+                end
+
+                racerData.lastLapClock = os.clock()
+
+                if racerData.lap > RaceController.cfg.laps then
+                    local overall = RaceController.secondsToClock(os.clock() - RaceController.startTime)
+                    local fastest = RaceController.secondsToClock(racerData.fastestLap)
+                    RaceController.broadcast(racer:getName() .. " has finished with an overall time of " .. overall .. " and a fastest lap of " .. fastest)
+                end
+            end
+        elseif os.clock() - lastNilVehicleWarn > 10 then
+            RaceController.broadcast("WARNING: " .. connections[racerID]:getName() .. " has nil vehicle! Laps will not be counted - reload your vehicle!")
+            lastNilVehicleWarn = os.clock()
+        end
+    end
+end)
+
+RaceController.print("Race controller loaded!")
---
+++ racecontroller/utils.lua
@@ -0,0 +1,184 @@
+function RaceController.print(...)
+    print("[RaceController] " .. ...)
+end
+
+function RaceController.msg(ply, ...)
+    ply:sendChatMessage("[RaceController] " .. table.concat({...}, " "))
+end
+
+function RaceController.broadcast(...)
+    send_message_broadcast("[RaceController] " .. table.concat({...}, " "))
+end
+
+function RaceController.writeCfg()
+    local cfgFile = io.open("./addons/racecontroller/config.json", "w")
+    cfgFile:write(encode_json_pretty(RaceController.cfg))
+    cfgFile:close()
+
+    RaceController.print("Successfully wrote configuration")
+end
+
+function RaceController.loadDefaultCfg()
+    RaceController.cfg = {
+        laps = 3,
+        minLapTime = 10,
+        finishLineWidth = 2,
+        linePositions = {},
+        admins = {}
+    }
+
+    RaceController.print("Default configuration loaded")
+end
+
+function RaceController.readCfg()
+    local f = io.open("./addons/racecontroller/config.json", "r")
+
+    if not f then
+        RaceController.loadDefaultCfg()
+        RaceController.writeCfg()
+        RaceController.print("Configuration file not found, new one created")
+
+        return
+    end
+
+    local cfg = decode_json(f:read("*all"))
+
+    if cfg then
+        RaceController.cfg = cfg
+        RaceController.print("Loaded configuration file")
+    else
+        RaceController.loadDefaultCfg()
+        RaceController.print("Configuration invalid! Fix or delete existing configuration and type reloadcfg in the console")
+    end
+
+    f:close()
+end
+
+function RaceController.addRacer(ply)
+    RaceController.racers[ply] = {
+        lap = 1,
+        lastLapTime = 0,
+        lastLapClock = 0, --os.clock() at which the last lap was completed
+        fastestLap = math.huge
+    }
+end
+
+function RaceController.removeRacer(ply)
+    RaceController.racers[ply] = nil
+end
+
+--https://stackoverflow.com/a/58411671
+local function round(n)
+    return n + (2 ^ 52 + 2 ^ 51) - (2 ^ 52 + 2 ^ 51)
+end
+
+function RaceController.secondsToClock(seconds)
+    local neg = seconds < 0
+    seconds = math.abs(seconds)
+    local hours = string.format("%02.f", math.floor(seconds / 3600))
+    local mins = string.format("%02.f", math.floor(seconds / 60 - (hours * 60)))
+    local secs = string.format("%02.f", math.floor(seconds - hours * 3600 - mins * 60))
+    local milisecs = string.format("%02.f", round((seconds % 1) * 100))
+
+    return (neg and "-" or "") .. mins .. ":" .. secs .. "." .. milisecs
+end
+
+function RaceController.isAdmin(ply)
+    return RaceController.cfg.admins[ply:getSecret()] and true or false
+end
+
+--Determines the distance between a point and a line segment - used for the start/finish line
+--https://stackoverflow.com/a/6853926
+function RaceController.pointToLineDist(x, y, x1, y1, x2, y2)
+    local A = x - x1
+    local B = y - y1
+    local C = x2 - x1
+    local D = y2 - y1
+    local dot = A * C + B * D
+    local len_sq = C * C + D * D
+    local param = -1
+
+    --in case of 0 length line
+    if len_sq ~= 0 then
+        param = dot / len_sq
+    end
+
+    local xx
+    local yy
+
+    if param < 0 then
+        xx = x1
+        yy = y1
+    elseif param > 1 then
+        xx = x2
+        yy = y2
+    else
+        xx = x1 + param * C
+        yy = y1 + param * D
+    end
+
+    local dx = x - xx
+    local dy = y - yy
+
+    return math.sqrt(dx * dx + dy * dy)
+end
+
+function RaceController.splitString(input, sep)
+    sep = sep or "%s"
+    local t = {}
+
+    for str in string.gmatch(input, "([^" .. sep .. "]+)") do
+        table.insert(t, str)
+    end
+
+    return t
+end
+
+--Returns a player connection object when given a full or partial name, not case sensitive
+function RaceController.findPlayerByName(name)
+    if not name then return end
+
+    for _, v in pairs(connections) do
+        if string.find(string.lower(v:getName()), string.lower(name), nil, true) then return v end
+    end
+end
+
+function RaceController.tableEmpty(tbl)
+    return next(tbl) == nil
+end
+
+RaceController.timers = {}
+
+function RaceController.timer(id, delay, reps, func)
+    RaceController.timers[id] = {
+        id = id,
+        delay = delay,
+        repsLeft = reps == 0 and math.huge or reps,
+        func = func,
+        lastRun = os.clock()
+    }
+end
+
+local timersMarkedForRemoval = {}
+
+hooks.register("Tick", "timerController", function()
+    for _, t in pairs(RaceController.timers) do
+        if os.clock() > t.lastRun + t.delay then
+            t.func()
+            t.lastRun = os.clock()
+            t.repsLeft = t.repsLeft - 1
+
+            if t.repsLeft == 0 then
+                table.insert(timersMarkedForRemoval, t.id)
+            end
+        end
+    end
+
+    for _, v in pairs(timersMarkedForRemoval) do
+        RaceController.timers[v] = nil
+    end
+
+    timersMarkedForRemoval = {}
+end)
+
+RaceController.print("Utils loaded!")
